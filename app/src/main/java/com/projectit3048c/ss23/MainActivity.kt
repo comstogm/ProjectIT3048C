fd669d5b03bc70a35707a2d1085d5e4060dc735e
gabriel <comstogm@mail.uc.edu>
2023-01-19 18:22:55 -0500

    Initial commit

---
+++ app/src/main/java/com/projectit3048c/ss23/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.projectit3048c.ss23
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.projectit3048c.ss23.ui.theme.ProjectIT3048CTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectIT3048CTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProjectIT3048CTheme {
+        Greeting("Android")
+    }
+}
