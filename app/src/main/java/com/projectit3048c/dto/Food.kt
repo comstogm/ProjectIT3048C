515ca8573d234a4caee6c7c6dfa0799397542732
Anastasiia Efimova <nastkaefff@mail.ru>
2023-02-28 11:28:25 -0500

    Dropdown fixed

--- app/src/main/java/com/projectit3048c/dto/Food.kt
+++ app/src/main/java/com/projectit3048c/dto/Food.kt
@@ -2,7 +2,7 @@ package com.projectit3048c.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Food(@SerializedName("id") val id: Int?, @SerializedName("name") val name: String="") {
+data class Food(@SerializedName("id") val id: Int=0, @SerializedName("name") val name: String="", @SerializedName("description") val description: String="", @SerializedName("calories") val calories: Int=0) {
     //@SerializedName("description") val description: String="", @SerializedName("calories") val calories: Int?){
 
     override fun toString(): String {
