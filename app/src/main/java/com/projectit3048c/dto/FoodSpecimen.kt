7da0d7dc209276d1cc0418d94d2bca29ac3f6cd4
gabriel <comstogm@mail.uc.edu>
2023-03-01 19:14:59 -0500

    Class changes, need to be var. Viewmodel changes, some name refactoring

---
+++ app/src/main/java/com/projectit3048c/dto/FoodSpecimen.kt
@@ -0,0 +1,4 @@
+package com.projectit3048c.dto
+
+data class FoodSpecimen(var id: Int=0, var name: String="", var description: String="", var calories: Int=0, var specimenID: String="", var date: String=""){
+}
