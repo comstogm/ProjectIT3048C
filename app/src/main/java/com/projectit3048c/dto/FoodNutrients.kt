4d9c059271b9bc1664ebfe595e8c48d1c908f3d6
chercher20 <93340674+chercher20@users.noreply.github.com>
2023-03-07 20:40:44 -0500

    Removed nothing phrase

--- app/src/main/java/com/projectit3048c/dto/FoodNutrients.kt
+++ app/src/main/java/com/projectit3048c/dto/FoodNutrients.kt
@@ -1,3 +1,2 @@
 package com.projectit3048c.dto
-
-data class FoodNutrients(var nutrientId: Int = 0, var nutrientName: String = "nothing", var nutrientNumber: Double = 0.0, var value: Double = 0.0)
+data class FoodNutrients(var nutrientId: Int = 0, var nutrientName: String = "", var nutrientNumber: Double = 0.0, var value: Double = 0.0)
