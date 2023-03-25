45dd0b488f996f5637f976ab0a976bd12a9e7a89
Anastasiia Efimova <nastkaefff@mail.ru>
2023-03-26 05:17:42 +0800

    Created Circle Progress bar in persentage

--- app/src/main/java/com/projectit3048c/dto/Food.kt
+++ app/src/main/java/com/projectit3048c/dto/Food.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class Food(var id: Int=0, var name: String="", var description: String="", var calories: Int=0) {
     override fun toString(): String {
-        return "$name"
+        return "$name $description $calories"
     }
 }
