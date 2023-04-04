4aee6845b21bf12c9b4278579669cfd99a6fe944
gabriel <comstogm@mail.uc.edu>
2023-04-03 21:58:19 -0400

    Trying to understand what is wrong with the spinner by recreating it.

--- app/src/main/java/com/projectit3048c/MainActivity.kt
+++ app/src/main/java/com/projectit3048c/MainActivity.kt
@@ -93,7 +93,39 @@ class MainActivity : ComponentActivity() {
                     CalorieFacts("Android", foods, foodAmounts, viewModel.selectedFoodAmount)
                 }
             }
-           }
+        }
+    }
+
+    @Composable
+    fun FoodAmountSpinner(foodAmountList: List<FoodAmount>) {
+        var specimenText by remember { mutableStateOf("Logged Foods") }
+        var expanded by remember { mutableStateOf(false) }
+        Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+            Row(Modifier
+                .padding(24.dp)
+                .clickable {
+                    expanded = !expanded
+                }
+                .padding(8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = specimenText, fontSize = 18.sp, modifier = Modifier.padding(end = 8.dp))
+                Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
+                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                    foodAmountList.forEach {
+                        DropdownMenuItem(onClick = {
+                            expanded = false
+                            specimenText = it.toString()
+                            viewModel.selectedFoodAmount = it
+                        }) {
+                            Text(text = it.toString())
+                        }
+                    }
+
+                }
+            }
+        }
     }
 
     @Composable
@@ -141,113 +173,29 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun TextFieldWithDropdownUsage(
-        dataIn: List<Food>?,
-        label: String = "",
-        take: Int = 3,
-        selectedFoodAmount: FoodAmount
-    ) {
-        val dropDownOptions = remember { mutableStateOf(listOf<Food>()) }
-        val textFieldValue =
-            remember(selectedFoodAmount.foodId) { mutableStateOf(TextFieldValue(selectedFoodAmount.foodName)) }
-        val dropDownExpanded = remember { mutableStateOf(false) }
-        fun onDropdownDismissRequest() {
-            dropDownExpanded.value = false
-        }
-
-        fun onValueChanged(value: TextFieldValue) {
-            inFoodName = value.text
-            dropDownExpanded.value = true
-            textFieldValue.value = value
-            dropDownOptions.value = dataIn?.filter {
-                it.toString().startsWith(value.text) && it.toString() != value.text
-            }!!.take(take)
-        }
-        TextFieldWithDropdown(
-            modifier = Modifier.fillMaxWidth(),
-            value = textFieldValue.value,
-            setValue = ::onValueChanged,
-            onDismissRequest = ::onDropdownDismissRequest,
-            dropDownExpanded = dropDownExpanded.value,
-            list = dropDownOptions.value,
-            label = label
-        )
-    }
-
-
-    @Composable
-    fun TextFieldWithDropdown(
-        modifier: Modifier = Modifier,
-        value: TextFieldValue,
-        setValue: (TextFieldValue) -> Unit,
-        onDismissRequest: () -> Unit,
-        dropDownExpanded: Boolean,
-        list: List<Food>,
-        label: String = ""
-    ) {
-        Box(modifier) {
-            TextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .onFocusChanged { focusState ->
-                        if (!focusState.isFocused)
-                            onDismissRequest()
-                    },
-                value = value,
-                onValueChange = setValue,
-                label = { Text(label) },
-                colors = TextFieldDefaults.outlinedTextFieldColors()
-            )
-            DropdownMenu(
-                expanded = dropDownExpanded,
-                properties = PopupProperties(
-                    focusable = false,
-                    dismissOnBackPress = true,
-                    dismissOnClickOutside = true
-                ),
-                onDismissRequest = onDismissRequest
-            ) {
-                list.forEach { text ->
-                    DropdownMenuItem(onClick = {
-                        setValue(
-                            TextFieldValue(
-                                text.toString(),
-                                TextRange(text.toString().length)
-                            )
-                        )
-                        selectedFood = text
-                    }) {
-                        Text(text = text.toString())
-                    }
-                }
-            }
-        }
-    }
-
-    @Composable
     fun CalorieFacts(
         name: String,
         foods: List<Food> = ArrayList<Food>(),
         loggedFoods: List<FoodAmount> = ArrayList<FoodAmount>(),
         selectedFoodAmount: FoodAmount = FoodAmount(),
     ) {
-        var inIntake by remember(selectedFoodAmount.foodIntake) { mutableStateOf(selectedFoodAmount.foodIntake) }
-        var inDate by remember(selectedFoodAmount.foodDate) { mutableStateOf(selectedFoodAmount.foodDate) }
-        var inAmount by remember(selectedFoodAmount.foodAmount) { mutableStateOf(selectedFoodAmount.foodAmount) }
+        var inIntake by remember(selectedFoodAmount.foodId) { mutableStateOf(selectedFoodAmount.foodIntake) }
+        var inDate by remember(selectedFoodAmount.foodId) { mutableStateOf(selectedFoodAmount.foodDate) }
+        var inAmount by remember(selectedFoodAmount.foodId) { mutableStateOf(selectedFoodAmount.foodAmount) }
         val context = LocalContext.current
         Column {
-            FoodAmountSpinner(loggedFoods = loggedFoods)
+            FoodAmountSpinner(foodAmountList = loggedFoods)
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier.fillMaxWidth()
             ){
                 CircleProgressBar(percentage = 0.8f, number = 100)
             }
-            TextFieldWithDropdownUsage(
-                dataIn = foods,
-                label = stringResource(R.string.foodName),
-                selectedFoodAmount = selectedFoodAmount
-            )
+//            TextFieldWithDropdownUsage(
+//                dataIn = foods,
+//                label = stringResource(R.string.foodName),
+//                selectedFoodAmount = selectedFoodAmount
+//            )
             OutlinedTextField(
                 value = inIntake,
                 onValueChange = { inIntake = it },
@@ -313,7 +261,7 @@ class MainActivity : ComponentActivity() {
         }
 
         @Composable
-        fun EventListItem(foodAmounts: FoodAmount) {
+        fun EventListItem2(foodAmounts: FoodAmount) {
             Row {
                 Column(Modifier.weight(6f)) {
                     Text(text = foodAmounts.foodId, style = typography.h6)
@@ -358,7 +306,10 @@ class MainActivity : ComponentActivity() {
         var inDescription by remember(photo.id) {mutableStateOf(photo.description)}
         Row{
             Column(Modifier.weight(2f)) {
-                AsyncImage(model = photo.localUri, contentDescription = "Event Image", Modifier.width(64.dp).height(64.dp))
+                AsyncImage(model = photo.localUri, contentDescription = "Event Image",
+                    Modifier
+                        .width(64.dp)
+                        .height(64.dp))
             }
             Column(Modifier.weight(4f)) {
                 Text(text = photo.id, style = typography.h6)
@@ -505,50 +456,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Composable
-    fun FoodAmountSpinner(loggedFoods: List<FoodAmount>) {
-        var loggedFoodText by remember { mutableStateOf("Logged Food Collection") }
-        var expanded by remember { mutableStateOf(false) }
-        Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-            Row(Modifier
-                .padding(24.dp)
-                .clickable {
-                    expanded = !expanded
-                }
-                .padding(8.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = loggedFoodText,
-                    fontSize = 18.sp,
-                    modifier = Modifier.padding(end = 8.dp)
-                )
-                Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
-                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                    loggedFoods.forEach { loggedFood ->
-                        DropdownMenuItem(onClick = {
-                            expanded = false
-
-                            if (loggedFood.foodName == (viewModel.NEW_FOODAMOUNT)) {
-                                // new specimen to create
-                                loggedFoodText = ""
-                            } else {
-                                loggedFoodText = loggedFood.toString()
-                                selectedFood = Food( name ="", description = "", calories = 0 )
-                                inFoodName = loggedFood.foodName
-
-                            }
-                            viewModel.selectedFoodAmount = loggedFood
-                            viewModel.fetchPhotos()
-
-                        }) {
-                            Text(text = loggedFood.toString())
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
