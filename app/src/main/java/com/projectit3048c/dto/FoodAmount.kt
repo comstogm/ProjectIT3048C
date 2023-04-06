f5753bed0097874a4e13f29ea269f89e36be3eca
westd5 <77344568+westd5@users.noreply.github.com>
2023-04-06 11:37:26 -0400

    Added KDoc to DAO & DTO files

--- app/src/main/java/com/projectit3048c/dto/FoodAmount.kt
+++ app/src/main/java/com/projectit3048c/dto/FoodAmount.kt
@@ -1,11 +1,26 @@
 package com.projectit3048c.dto
 
+/**
+ * Data class representing an amount of food
+ *
+ * @property foodId ID of the food
+ * @property foodName Name of food
+ * @property internalFoodID ID of food from internal database
+ * @property description Description of food
+ * @property calories Number of calories in food
+ */
 data class FoodAmount(var foodId: String = "", //firebase identifier
                       var foodName: String = "",
                       var internalFoodID: Int = 0,
                       var foodIntake: String = "",
                       var foodAmount: String = "",
                       var foodDate: String = "") {
+
+    /**
+     * Returns a string of food and its intake amount
+     *
+     * @return String made from the name/amount/date of the food
+     */
     override fun toString(): String {
         return "$foodName $foodAmount $foodDate"
     }
