e8f924f8e211eeb354eb5037b913f6b385b3733e
Anastasiia Efimova <nastkaefff@mail.ru>
2023-04-07 11:05:45 -0400

    changed calendar fun, added button padding

--- app/src/main/java/com/projectit3048c/dto/Food.kt
+++ app/src/main/java/com/projectit3048c/dto/Food.kt
@@ -2,8 +2,8 @@ package com.projectit3048c.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Food(var id: Int=0, var name: String="", var description: String="", var calories: Int = 0) {
+data class Food(var id: Int=0, var name: String="", var description: String="", var calories: Float = 0.0f) {
     override fun toString(): String {
-        return "$name $description $calories"
+        return "$name $description"
     }
 }
